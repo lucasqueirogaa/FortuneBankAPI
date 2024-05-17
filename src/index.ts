b8adb2c55fb91ff342f587112dbe94c5df18c292
Lucas Queiroga <lucasqueirogadev@gmail.com>
2024-05-17 18:30:25 -0300

    chore: Change app base. From `express` to `fastify`

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
-import api from "./api";
-import { connectDB } from "./config/db";
+import 'dotenv/config'
+import Fastify from "fastify";
 import { Logger } from "./config/logger";
+import { connectDB } from './config/db';
 
 const dbUri = process.env.DB_URI || "";
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
+
+
+const server = Fastify({});
+
+server.get("/", async (request, reply) => {
+  Logger.info("Base route called");
+  
+  return reply.code(200).send("API working");
+});
 
 connectDB(dbUri);
-
-api.listen(port, () => {
-  Logger.info(`A conexão foi estabelecida com sucesso na porta: ${port}`);
-});
+try {
+  server.listen({ port });
+  Logger.info(`Connected with on port ${port}`);
+} catch (e: any) {
+  Logger.error(`Error with fastify: ${e}`);
+}
