d5dae936224f069e439f05e57ee8786d171bbfff
Lucas Queiroga <lucasqueirogadev@gmail.com>
2024-05-18 17:00:59 -0300

    feat: Add `initial test` to verify setup works

---
+++ src/tests/users/BaseTest.test.ts
@@ -0,0 +1,7 @@
+import { test, expect, describe } from "vitest";
+
+describe("Base test", () => {
+  test("Check if vitest is OK, return true", async () => {
+    expect(true).toBe(true);
+  });
+});
--- src/tests/users/Users.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect, describe } from "vitest";
-
-describe("User service", () => {
-  test("Deve ser possível criar um usuário", async () => {
-    expect(true).toBe(true);
-  });
-});
