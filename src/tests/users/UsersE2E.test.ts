bb78a673f299c2fca90b9260e61b086d766cf49d
Lucas Queiroga <lucasqueirogadev@gmail.com>
2024-05-19 09:19:40 -0300

    feat: Create `tests` to create user

--- src/tests/users/UsersE2E.test.ts
+++ src/tests/users/UsersE2E.test.ts
@@ -1,4 +1,4 @@
-import { test, expect, describe, beforeAll, afterAll } from "vitest";
+import { test, describe, beforeAll, afterAll } from "vitest";
 import request from "supertest";
 import { server as app } from "../../api/routes";
 import { connectDB, disconnectDB } from "../../config/db";
@@ -19,11 +19,71 @@ describe("User E2E", () => {
       .post("/user")
       .send({
         name: "Lucas Queiroga",
-        cpf: "12341293131",
+        cpf: "12345678910",
         age: 18,
-        email: "lucas2@gmail.com",
+        email: "lucas@gmail.com",
         password: "13452131212",
       })
       .expect(201);
+  });
+
+  test("User should't be create with same CPF", async () => {
+    await app.ready();
+
+    await request(app.server)
+      .post("/user")
+      .send({
+        name: "Lucas Queiroga",
+        cpf: "12345678910",
+        age: 18,
+        email: "lucas1@gmail.com",
+        password: "13452131212",
+      })
+      .expect(400);
+  });
+
+  test("User should't be create with same email", async () => {
+    await app.ready();
+
+    await request(app.server)
+      .post("/user")
+      .send({
+        name: "Lucas Queiroga",
+        cpf: "12345678911",
+        age: 18,
+        email: "lucas@gmail.com",
+        password: "13452131212",
+      })
+      .expect(400);
+  });
+
+  test("User should't be create with age under 18", async () => {
+    await app.ready();
+
+    await request(app.server)
+      .post("/user")
+      .send({
+        name: "Lucas Queiroga",
+        cpf: "12345678914",
+        age: 17,
+        email: "lucas2@gmail.com",
+        password: "13452131212",
+      })
+      .expect(400);
+  });
+
+  test("User should't be create with missing informations", async () => {
+    await app.ready();
+
+    await request(app.server)
+      .post("/user")
+      .send({
+        name: "",
+        cpf: "12345678914",
+        age: 17,
+        email: "lucas2@gmail.com",
+        password: "13452131212",
+      })
+      .expect(400);
   });
 });
